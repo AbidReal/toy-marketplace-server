4e0c4911c39a7aacd9127df62bbb7c59b7e49271
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-11-30 22:43:43 +0600

    cors updated website

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors({}));
+app.use(cors({ origin: "https://toy-marketplace-a7ee2.web.app" }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7vnkrh3.mongodb.net/?retryWrites=true&w=majority`;
